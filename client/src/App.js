4a9fbfbb323dfb6e695db51ca49c5748f8b2277b
Ryan Goderis <rgoderis789@gmail.com>
2019-11-01 15:33:12 -0400

    created Login page that has a function of login that checks to see if a user is loged in, imported LoginCard component to Login that calls login onLogin and redirects to username/trending

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 import { BrowserRouter as Router, Route, Redirect, Link, Switch, withRouter } from "react-router-dom";
 
 function App() {
---
+++ client/src/components/Landing/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Landing (){
+    return(
+        <div className="jumbotron">
+            <h1 className="text-center">mWatch</h1>
+        </div>
+    )
+}
+
+export default Landing
