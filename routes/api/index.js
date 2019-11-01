2adcdcf65da8347cbd6b3056bd7bb53bf84956bb
Ryan Goderis <rgoderis789@gmail.com>
2019-11-01 11:29:38 -0400

    created user model, added passport authentication to user model, mWatch controller, and users api

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const bookRoutes = require("./books");
+const userRoutes = require("./users.js");
 
 // Book routes
 router.use("/books", bookRoutes);
+// User Routes
+router.use("/users", userRoutes);
+
 
 module.exports = router;
---
+++ routes/api/users.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const passport = require("passport");
+const userController = require("../../controllers/mWatch");
+
+router.route("/")
+    .get(userController.getUser);
+router.route("/signup")
+    .post(userController.register);
+router.route("/logn")
+    .post(passport.authenticate("local", {failureRedirect: "/login"}), userController.login);
+router.route("logout")
+    .get(userController.logout);
+
+module.exports = router
