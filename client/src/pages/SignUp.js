49f9954b5d25de73a4d9c60a232b76f11d6ba729
Ryan Goderis <rgoderis789@gmail.com>
2019-11-03 09:55:26 -0500

    created class of SignUp in SignUp page, set function of register to perform POST request with data from SignUpCard, in SignUpCard set signup form to take username and password

---
+++ client/src/pages/SignUp.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import SignUpCard from "../components/SignUpCard";
+//The component for doing the actual signup of the User
+class SignUp extends React.Component {
+	state = {
+		redirectToReferrer: false
+	}
+
+	register = (data) => {
+		fetch('api/users/register', {
+			method: 'POST',
+			body: JSON.stringify(data),
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			credentials: 'include'
+		})
+		.then((response) => {
+			if (response.status === 200) {
+				console.log('Succesfully registered user!');
+				//relocate to the login page
+				window.location.assign("/protected");
+			}
+		})
+		.catch((err) => {
+			console.log('Error registering user.', err);
+		});
+	}
+
+	render() {
+		return (
+			<div>
+                <Landing/>
+				<h4>Register a New User</h4>
+				<SignUpCard onRegister={this.register} />
+			</div>
+		)
+	}
+}
+
+export default SignUp
