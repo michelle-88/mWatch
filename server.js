b763ba54df1bfafeef380e6d9ff93b95fa6589e7
Ryan Goderis <rgoderis789@gmail.com>
2019-11-06 11:58:01 -0500

    removed duplicate path declaration

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(flash());
 if (process.env.NODE_ENV === "production") {
   app.use(passport.session()); app.use(express.static(path.join(__dirname, '../build')));
     // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
+    // const path = require('path');
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
